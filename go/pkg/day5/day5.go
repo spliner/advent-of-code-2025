bf31c1166ff969604150c6ce20fa71708d98fad6
Fillipe Rosini <hi@frosini.dev>
2025-12-05 12:41:03 -0300

    Day 5 in Go

---
+++ go/pkg/day5/day5.go
@@ -0,0 +1,117 @@
+package day5
+
+import (
+	"bufio"
+	"fmt"
+	"slices"
+	"strconv"
+	"strings"
+)
+
+type Range struct {
+	Start uint64
+	End   uint64
+}
+
+func Part1(scanner *bufio.Scanner) (string, error) {
+	ranges, err := parseRanges(scanner)
+	if err != nil {
+		return "", fmt.Errorf("failed to parse ranges: %w", err)
+	}
+
+	// Ids
+	var count int
+	for scanner.Scan() {
+		line := strings.TrimSpace(scanner.Text())
+		if line == "" {
+			break
+		}
+
+		id, err := strconv.ParseUint(line, 10, 64)
+		if err != nil {
+			return "", fmt.Errorf("failed to parse id: %s", line)
+		}
+
+		for _, r := range ranges {
+			if id >= r.Start && id <= r.End {
+				count++
+				break
+			}
+		}
+	}
+	if err := scanner.Err(); err != nil {
+		return "", fmt.Errorf("failed to read ids: %w", err)
+	}
+
+	return strconv.Itoa(count), nil
+}
+
+func parseRanges(scanner *bufio.Scanner) ([]Range, error) {
+	ranges := make([]Range, 0, 100)
+
+	for scanner.Scan() {
+		line := strings.TrimSpace(scanner.Text())
+		if line == "" {
+			break
+		}
+
+		startStr, endStr, ok := strings.Cut(line, "-")
+		if !ok {
+			return nil, fmt.Errorf("unexpected range: %s", line)
+		}
+
+		start, err := strconv.ParseUint(startStr, 10, 64)
+		if err != nil {
+			return nil, fmt.Errorf("failed to parse range start: %s", startStr)
+		}
+
+		end, err := strconv.ParseUint(endStr, 10, 64)
+		if err != nil {
+			return nil, fmt.Errorf("failed to parse range end: %s", endStr)
+		}
+
+		ranges = append(ranges, Range{Start: start, End: end})
+	}
+	if err := scanner.Err(); err != nil {
+		return nil, fmt.Errorf("failed to read ranges: %w", err)
+	}
+
+	return ranges, nil
+}
+
+func Part2(scanner *bufio.Scanner) (string, error) {
+	ranges, err := parseRanges(scanner)
+	if err != nil {
+		return "", fmt.Errorf("failed to parse ranges: %w", err)
+	}
+
+	slices.SortFunc(ranges, func(a, b Range) int {
+		if a.Start < b.Start {
+			return -1
+		} else if a.Start > b.Start {
+			return 1
+		}
+		return 0
+	})
+
+	nonOverlappingRanges := make([]Range, 0, len(ranges))
+	var previousRange Range
+	for _, r := range ranges {
+		start := max(r.Start, previousRange.End)
+		if start == previousRange.End {
+			start += 1
+		}
+		end := max(r.End, previousRange.End)
+		if end < start {
+			continue
+		}
+		nonOverlappingRanges = append(nonOverlappingRanges, Range{Start: start, End: end})
+		previousRange = r
+	}
+
+	var sum uint64
+	for _, r := range nonOverlappingRanges {
+		sum += r.End - r.Start + 1
+	}
+	return strconv.FormatUint(sum, 10), nil
+}
---
+++ go/pkg/day5/day5_test.go
@@ -0,0 +1,49 @@
+package day5_test
+
+import (
+	"aoc2025/pkg/assert"
+	"aoc2025/pkg/day5"
+	"bufio"
+	"strings"
+	"testing"
+)
+
+func TestPart1(t *testing.T) {
+	input := `3-5
+10-14
+16-20
+12-18
+
+1
+5
+8
+11
+17
+32`
+	scanner := bufio.NewScanner(strings.NewReader(input))
+
+	result, err := day5.Part1(scanner)
+
+	assert.Nil(t, err)
+	assert.Equal(t, "3", result)
+}
+
+func TestPart2(t *testing.T) {
+	input := `3-5
+10-14
+16-20
+12-18
+
+1
+5
+8
+11
+17
+32`
+	scanner := bufio.NewScanner(strings.NewReader(input))
+
+	result, err := day5.Part2(scanner)
+
+	assert.Nil(t, err)
+	assert.Equal(t, "14", result)
+}
