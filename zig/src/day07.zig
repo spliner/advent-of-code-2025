d8ceccdebb5c0e80545464aabc314d6e0faad0b3
Fillipe Rosini <hi@frosini.dev>
2025-12-19 17:57:58 -0300

    Day 7 part 2 in zig

--- zig/src/day07.zig
+++ zig/src/day07.zig
@@ -71,8 +71,33 @@ pub fn part1(allocator: std.mem.Allocator, reader: *std.Io.Reader) !day.Answer {
     return day.Answer{ .int = @intCast(splits) };
 }
 
-pub fn part2(_: std.mem.Allocator, _: *std.Io.Reader) !day.Answer {
-    return day.Answer{ .int = 0 };
+pub fn part2(allocator: std.mem.Allocator, reader: *std.Io.Reader) !day.Answer {
+    var it = LineIterator.init(allocator, reader);
+    defer it.deinit();
+
+    const first_line = try it.next() orelse unreachable;
+
+    var beams = try std.ArrayList(usize).initCapacity(allocator, first_line.len);
+    defer beams.deinit(allocator);
+
+    for (first_line) |c| {
+        const val: usize = if (c == 'S') 1 else 0;
+        try beams.append(allocator, val);
+    }
+
+    var timelines: usize = 1;
+    while (try it.next()) |line| {
+        for (beams.items, 0..) |val, i| {
+            if (val != 0 and line[i] == '^') {
+                timelines += val;
+                beams.items[i - 1] += val;
+                beams.items[i + 1] += val;
+                beams.items[i] = 0;
+            }
+        }
+    }
+
+    return day.Answer{ .int = @intCast(timelines) };
 }
 
 test "part1" {
@@ -132,5 +157,5 @@ test "part2" {
 
     const result = try part2(std.testing.allocator, &reader.interface);
 
-    try std.testing.expectEqual(day.Answer{ .int = 0 }, result);
+    try std.testing.expectEqual(day.Answer{ .int = 40 }, result);
 }
